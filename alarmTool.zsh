f67b551d9ba6321de9d038836762afa4e85ce0ea
“Luna-stella-831” <kazukiclclclimax31@gmail.com>
2023-04-01 14:17:18 +0900

    make repo for Crypton

---
+++ alarmTool.zsh
@@ -0,0 +1,2 @@
+osascript /Users/hasegawakazuteru/job_hunting/Crypton/本選考/alarmTool/alarmTool.scpt
+date "+%Y.%m.%d  %H:%M:%S" >> /Users/hasegawakazuteru/job_hunting/Crypton/本選考/alarmTool/alarmTool.log
